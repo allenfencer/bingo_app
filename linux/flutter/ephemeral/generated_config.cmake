50572f2456187144aaf99eef101734401eaeaca3
Ashik-2001 <ashikbabu2001@gmail.com>
2022-11-06 06:58:10 +0530

    Updating changes to support it in linux env

---
+++ linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,15 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/ashik/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/ashik/work-space/flutter/bingo_new/bingo_app" PROJECT_DIR)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/ashik/flutter"
+  "PROJECT_DIR=/home/ashik/work-space/flutter/bingo_new/bingo_app"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/ashik/work-space/flutter/bingo_new/bingo_app/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/ashik/work-space/flutter/bingo_new/bingo_app/lib/main.dart"
+)
